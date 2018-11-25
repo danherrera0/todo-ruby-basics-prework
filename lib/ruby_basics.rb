bf6362699f688e75ff41c28fa3146c84ea6ab3f5
Dania Herrera <danherrera0@gmail.com>
2018-11-25 16:48:39 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+
 end
 
 def assign_variable(value)
-  
+
 end
 
 def argue
-  
+
 end
 
-def greeting
+def greeting(phrase,name)
 
 end
 
 def return_a_value
+  puts "Nice"
 
 end
 
 def last_evaluated_value
-
+return "expert"
 end
 
-def pizza_party
+def pizza_party(topping="cheese")
+  return topping
 
 end
